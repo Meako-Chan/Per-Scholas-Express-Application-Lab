de939d2a2d3fe9fbe851dd39c06b2353e8f356ab
Michael Chan <michael.w.chan01@gmail.com>
2024-07-09 00:33:07 -0400

    Create different routes

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
+/* GET user listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('user', {message: 'Hello!'});
 });
 
 module.exports = router;
